5da1442e53d2f3eb2cdbe07a09112c8003fe45a2
MasonStokes <MasonStokes25@gmail.com>
2019-02-01 17:21:38 -0800

    scaffold crud

---
+++ Documents/DiscussionProject/DiscussionProject/Models/SUPDiscussion.cs
@@ -0,0 +1,39 @@
+namespace DiscussionProject.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class SUPDiscussion
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public SUPDiscussion()
+        {
+            SUPComments = new HashSet<SUPComment>();
+        }
+
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string URL { get; set; }
+
+        public DateTime TIMESTAMP { get; set; }
+
+        public int UserID { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SUPComment> SUPComments { get; set; }
+
+        public virtual SUPUser SUPUser { get; set; }
+    }
+}
