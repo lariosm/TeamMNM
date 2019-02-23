6c87b8880af0088ff1610f248f06a8dbce58c001
MasonStokes <MasonStokes25@gmail.com>
2019-02-23 14:30:03 -0800

    refactor create item redirect

--- URent/URent/Models/SUPContext.cs
+++ URent/URent/Models/SUPContext.cs
@@ -8,7 +8,7 @@ namespace URent.Models
     public partial class SUPContext : DbContext
     {
         public SUPContext()
-            : base("name=SUPUserTables")
+            : base("name=MasonLocal")
         {
         }
 
