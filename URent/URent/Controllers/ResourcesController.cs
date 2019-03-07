d92d41dc4d928fc32c68d12ed7ec28dfd0302fd1
nki13 <nki13@wou.edu>
2019-03-06 16:24:02 -0800

    added resources controller for usage when showing photos on item details page

---
+++ URent/URent/Controllers/ResourcesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace URent.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        // GET: Resources
+        public ActionResult Index(int? id)
+        {
+            
+            return View();
+        }
+    }
+}
