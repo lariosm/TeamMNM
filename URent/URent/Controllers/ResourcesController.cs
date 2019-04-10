52a23d5b33b541a8481af7c7e6c097154c819f66
nki13 <nki13@wou.edu>
2019-04-10 13:40:52 -0700

    added ajax for notifications, not working yet.

--- URent/URent/Controllers/ResourcesController.cs
+++ URent/URent/Controllers/ResourcesController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using URent.Models;
@@ -12,6 +14,25 @@ namespace URent.Controllers
     public class ResourcesController : Controller
     {
         SUPContext db = new SUPContext();
+
+        [Authorize]
+        private string getIdentityID()
+        {
+            return User.Identity.GetUserId();
+        }
+
+        /// <summary>
+        /// Retrieves user ID of current user from SUPUsers table that is associated with user ID from AspNetUsers table.
+        /// </summary>
+        /// <returns>User ID of current user from SUPUsers table associated with user ID from AspNetUsers table.</returns>
+        [Authorize]
+        private int getSUPUserID()
+        {
+            string id = getIdentityID();
+            SUPUser supUser = db.SUPUsers.Where(u => u.NetUserId.Equals(id)).FirstOrDefault();
+            int supUserid = supUser.Id;
+            return supUserid;
+        }
 
         /// <summary>
         /// Retrieves photo to display to view.
@@ -26,5 +47,13 @@ namespace URent.Controllers
             //Image file = Image.FromStream(stream);
             return File(p.Input, "image");
         }
+
+        public JsonResult NotificationRequest()
+        {
+            int id = getSUPUserID(); //Retrieve ID of current user.
+            var notifications = db.SUPTransactions.Where(u => u.OwnerID == id).OrderBy(b => b.TimeStamp).ToList(); //Find all item listings that is requested/rented from other users
+            return Json(notifications, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
---
+++ URent/URent/Scripts/UserNotifications.js
@@ -0,0 +1,37 @@
+﻿function notificationRequest() {
+    var source = "/Resources/NotificationRequest"
+
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: source,
+        success: showNotifications,
+        error: ajaxError
+    })
+}
+
+function showNotifications(notificationList) {
+    if (notificationList.length == 0) {
+        $("#message").empty();
+        $("#message").append("No notifications to display.")
+    }
+    else {
+        $("#message").empty();
+        $(".notification-info").remove();
+        for (var i = 0; i < notificationList.length; i++) {
+            $("#notificationTable").prepend("<tr class=\notification-info\"><td>" + notificationList[i] + "</td></tr>");
+        }
+    }
+}
+
+function ajaxError() {
+    alert("Unable to reach server. Please try again.");
+}
+
+function main() {
+    notificationRequest();
+    var interval = 1000 * 5;
+    window.setInterval(notificationRequest, interval);
+}
+
+$(document).ready(main());
