e5fc02ff720196a1dd59b9553c8feb182d31597e
MasonStokes <MasonStokes25@gmail.com>
2019-04-25 13:40:19 -0700

    update item controller to test soon

--- URent/URent/Models/SUPContext.cs
+++ URent/URent/Models/SUPContext.cs
@@ -1,9 +1,11 @@
+using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
 namespace URent.Models
 {
-    using System;
-    using System.Data.Entity;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Linq;
+    
 
     public partial class SUPContext : DbContext
     {
