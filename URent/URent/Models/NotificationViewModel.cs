7dfe2da9cc0f90dc422404027a67f09ed9c10546
Manuel Larios <mlarios12@wou.edu>
2019-05-01 21:09:38 -0700

    Added AJAX functionality to Notifications page

---
+++ URent/URent/Models/NotificationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace URent.Models
+{
+    public class NotificationViewModel
+    {
+        public List<SUPTransaction> Notifications { get; set; }
+
+        public Dictionary<int, string> RenterNames { get; set; }
+    }
+}
--- URent/URent/Scripts/UserNotifications.js
+++ URent/URent/Scripts/UserNotifications.js
@@ -6,27 +6,49 @@
         dataType: "json",
         url: source,
         success: showNotifications,
-        error: ajaxError
+        error: notificationError
     })
 }
 
 function showNotifications(notificationList) {
-    console.log("In show notifications method");
+    console.log(notificationList);
+
     if (notificationList.length == 0) {
         $("#message").empty();
-        $("#message").append("No notifications to display.")
+        $("#message").append("No notifications to display")
     }
     else {
         $("#message").empty();
         $(".notification-info").remove();
         for (var i = 0; i < notificationList.length; i++) {
-            $("#notiftable").prepend("<tr class=\notification-info\"><td>" + notificationList[i].item + "</td></td>" + notificationList[i.renter] + "</td></td>" + notificationList[i].start + "</td></td>" + notificationList[i].end + "</td></td>" + notificationList[i].time + "</td></td>" + notificationList.price + "</td></tr>");
+            $(".table").append("<tr class=\"notification-info\"><td>" + notificationList[i].ItemName + "</td><td>" + notificationList[i].FirstName + " " + notificationList[i].LastName + "</td><td>" + formatJSONDate(notificationList[i].StartDate) + "</td><td>" + formatJSONDate(notificationList[i].EndDate) + "</td><td>" + formatJSONDate(notificationList[i].TimeStamp) + "</td><td>$" + Number(notificationList[i].TotalPrice).toLocaleString('en-US', { minimumFractionDigits: 2 }) + "</td></tr>");
         }
     }
 }
 
-function ajaxError() {
-    alert("Unable to reach server. Please try again.");
+function notificationError() {
+    alert("Unable to reach server. Please try again later.");
+}
+
+function formatJSONDate(jsonDate) {
+    //Formats Microsoft JSON date (i.e. /Date(1556686986603)/) into a date object that JavaScript can read
+    var parsedDate = new Date(parseInt(jsonDate.substr(6)));
+
+    //Converts date object to MM/DD/YYYY format
+    var mm = parsedDate.getMonth() + 1; //January is 0!
+    var dd = parsedDate.getDate();
+    var yyyy = parsedDate.getFullYear();
+
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+
+    var date = mm + '/' + dd + '/' + yyyy;
+
+    return date;
 }
 
 function main() {
@@ -35,4 +57,4 @@ function main() {
     window.setInterval(notificationRequest, interval);
 }
 
-$(document).ready(main());
+$(document).ready(main);
