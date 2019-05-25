efe8d80a868fc01e9e80481ba8325f626b8915ed
Manuel Larios <mlarios12@wou.edu>
2019-05-25 09:11:03 -0700

    Fixed JS error in register page

--- URent/URent/App_Start/BundleConfig.cs
+++ URent/URent/App_Start/BundleConfig.cs
@@ -19,7 +19,7 @@ namespace URent
                         "~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.min.js"));
+                        "~/Scripts/jquery-ui-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
